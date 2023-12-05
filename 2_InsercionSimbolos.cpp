b6cd2f61fcaeda5a675854190e447e291d834756
jsepucas <jsepucas@myuax.com>
2023-12-05 11:46:33 +0100

    2.7 (comentarios)

--- 2_InsercionSimbolos.cpp
+++ 2_InsercionSimbolos.cpp
@@ -5,13 +5,20 @@
 #include <variant>
 
 using namespace std;
+//Clase para gestionar las variables
 
 class Entorno {
 public:
     Entorno() = default;
 
+    // Evito que se pueda copiar la clase para prevenir duplicados no deseados.
+
+
     Entorno(const Entorno&) = delete;
     Entorno& operator=(const Entorno&) = delete;
+
+    // Intento insertar una nueva variable. Devuelvo true si se inserta correctamente.
+
 
     bool insert(const std::string& name, const std::variant<int, double, std::string>& value) {
         auto result = simbolos.insert({name, value});
@@ -20,6 +27,8 @@ public:
     void setVariable(const std::string& name, const std::variant<int, double, std::string>& value) {
         simbolos[name] = value;
     }
+
+    // Establezco o actualizo el valor de una variable.
 
     template<typename T>
     T getVariable(const std::string& name) const {
@@ -43,9 +52,19 @@ int main() {
     Entorno gameEnvironment;
 
 
-    bool inserted = gameEnvironment.insert("VidaJugador", 100);
+    bool inserted = gameEnvironment.insert("VidaJugador", 200   );
    cout << "'vidaJugador' ha sido insertada? " << (inserted ? "Si" : "No") << std::endl;
 
+    inserted = gameEnvironment.insert("vidaJugador", 200);
+    cout << "Intento de reinsertar 'vidaJugador'? " << (inserted ? "Si" : "No") << endl;
 
+    try {
+        cout << "Vida del Jugador: " << gameEnvironment.getVariable<int>("VidaJugador") << std::endl;
+    } catch (const std::runtime_error& e) {
+      cerr << "Error: " << e.what() << endl;
+    }
+
+    return 0;
+}
 
 
