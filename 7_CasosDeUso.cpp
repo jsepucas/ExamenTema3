a8306528b9f8da86790336007e6703ca59109786
jsepucas <jsepucas@myuax.com>
2023-12-05 12:28:15 +0100

    7.2

--- 7_CasosDeUso.cpp
+++ 7_CasosDeUso.cpp
@@ -23,3 +23,37 @@ public:
             iter->second.first = value;
         }
     }
+
+    template<typename T>
+    T getVariableAs(const string& name) const {
+        auto it = simbolos.find(name);
+        if (it == simbolos.end()) {
+            throw runtime_error("La variable del juego '" + name + "' no existe");
+        }
+        try {
+            return get<T>(it->second.first);
+        } catch (const bad_variant_access&) {
+            throw runtime_error("Tipo incorrecto para la variable '" + name + "'");
+        }
+    }
+private:
+    map<string, pair<Value, bool>> simbolos; // Almacenar variables con una marca de solo lectura
+};
+
+int main() {
+    Entorno gameEnv;
+
+    gameEnv.setVariable("Dificultad", string("Dificil"));
+    gameEnv.setVariable("Salud", 200);
+    gameEnv.setVariable("Puntos", 10000);
+    private:
+    map<string, pair<Value, bool>> simbolos; // Almacenar variables con una marca de solo lectura
+};
+
+int main() {
+    Entorno gameEnv;
+
+    gameEnv.setVariable("Dificultad", string("Normal"));
+    gameEnv.setVariable("Salud", 100);
+    gameEnv.setVariable("Puntos", 5000);
+
