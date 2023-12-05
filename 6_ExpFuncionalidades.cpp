13bab7df303ffeaf9d552b5153ffa5d0e28a3b21
jsepucas <jsepucas@myuax.com>
2023-12-05 12:14:52 +0100

    6.1

--- 6_ExpFuncionalidades.cpp
+++ 6_ExpFuncionalidades.cpp
@@ -4,3 +4,14 @@
 #include <variant>
 #include <stdexcept>
 #include<vector>
+
+using namespace std;
+
+class Entorno {
+public:
+    using Value = variant<int, double, std::string>;
+
+    Entorno() = default;
+
+    Environment(const Entorno&) = delete;
+    Entorno& operator=(const Entorno&) = delete;
