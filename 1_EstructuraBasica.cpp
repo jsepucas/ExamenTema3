909d1317bb668713570609ab777aa2afb48b6d2e
jsepucas <jsepucas@myuax.com>
2023-12-05 11:29:29 +0100

    10 (Comentarios 1)

--- 1_EstructuraBasica.cpp
+++ 1_EstructuraBasica.cpp
@@ -6,12 +6,18 @@
 
 using namespace std;
 
+//Creo una clase Entorno para gestionar las variables
+
 class Entorno {
 public:
     Entorno () = default;
 
+    //Elimino el constructor de copia y el operador de asignación para evitar copias que no quiero
+
     Entorno (const Entorno&) = delete;
     Entorno& operator=(const Entorno&) = delete;
+
+    //Creo una función para asignar variables
 
     void setVariable(const string& nombre, const variant<int, double, string>& valor) {
         simbolos[nombre] = valor;
@@ -20,8 +26,8 @@ public:
 
     template<typename T>
     T getVariable(const string& name) const {
-        auto it = symbols.find(name);
-        if (it != symbols.end()) {
+        auto it = simbolos.find(name);
+        if (it != simbolos.end()) {
             try {
                 return get<T>(it->second);
             } catch (const bad_variant_access&) {
@@ -38,12 +44,15 @@ private:
 
 int main() {
     Entorno gameEnvironment;
-
-    gameEnvironment.setVariable("VidaJugador", 100);
-    gameEnvironment.setVariable("Puntuacion", 0);
+// Establezco las distintas variables
+    gameEnvironment.setVariable("VidaJugador", 200);
+    gameEnvironment.setVariable("Puntuacion", 10);
     gameEnvironment.setVariable("NombreNivel", string("Casa Abandonada"));
 
+//Try catch para capturar los errores
     try {
+
+        //Valor de las variables
         cout << "Vida del Jugador: " << gameEnvironment.getVariable<int>("VidaJugador") << endl;
         cout << "Puntuacion: " << gameEnvironment.getVariable<int>("Puntuacion") << endl;
         cout << "Nombre del Nivel: " << gameEnvironment.getVariable<string>("NombreNivel") << endl;
@@ -51,6 +60,8 @@ int main() {
         cerr << "Error: " << e.what() << endl;
     }
 
+    return 0;
+}
 
 
 
@@ -58,4 +69,4 @@ int main() {
 
 
 
-};
+
