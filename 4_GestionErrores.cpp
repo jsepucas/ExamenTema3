ec2e52323d33db012c944650c1a3d417e3fe5ddb
jsepucas <jsepucas@myuax.com>
2023-12-05 12:01:38 +0100

    4.1

--- 4_GestionErrores.cpp
+++ 4_GestionErrores.cpp
@@ -15,4 +15,13 @@ public:
     Entorno(const Entorno&) = delete;
     Entorno& operator=(const Entorno&) = delete;
 
+    void setVariable(const string& name, const Value& value, bool readOnly = false) {
+        auto [iter, success] = simbolos.insert({name, make_pair(value, readOnly)});
+        if (!success && iter->second.second) {
+            throw runtime_error("No se puede modificar la constante del juego '" + name + "'");
+        } else if (!success) {
+            iter->second.first = value; // Actualizo el valor de la variable
+        }
+    }
+
 
