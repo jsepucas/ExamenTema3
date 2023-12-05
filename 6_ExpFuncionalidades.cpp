eb4c777af94e0a1fdcf404aab871908fa5bfb368
jsepucas <jsepucas@myuax.com>
2023-12-05 12:15:52 +0100

    6.2

--- 6_ExpFuncionalidades.cpp
+++ 6_ExpFuncionalidades.cpp
@@ -13,5 +13,14 @@ public:
 
     Entorno() = default;
 
-    Environment(const Entorno&) = delete;
+    Entorno(const Entorno&) = delete;
     Entorno& operator=(const Entorno&) = delete;
+
+    void setVariable(const string& name, const Value& value, bool readOnly = false) {
+        auto [iter, success] = symbols.insert({name, make_pair(value, readOnly)});
+        if (!success && iter->second.second) {
+            throw runtime_error("No se puede modificar la constante del juego '" + name + "'");
+        } else if (!success) {
+            iter->second.first = value;
+        }
+    }
