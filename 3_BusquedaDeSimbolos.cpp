793e3f339cd65d74f8c400f13a711186ab0da242
jsepucas <jsepucas@myuax.com>
2023-12-05 11:51:51 +0100

    3.2

--- 3_BusquedaDeSimbolos.cpp
+++ 3_BusquedaDeSimbolos.cpp
@@ -10,3 +10,7 @@ class Entorno {
 public:
     using Value = variant<int, double, string>;
 
+    Entorno() = default;
+
+    Entorno(const Entorno&) = delete;
+    Entorno& operator=(const Entorno&) = delete;
