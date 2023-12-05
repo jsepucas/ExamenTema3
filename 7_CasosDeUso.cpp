26ef95084803461b54087cff0646a12230d24e7a
jsepucas <jsepucas@myuax.com>
2023-12-05 12:27:05 +0100

    7.1

---
+++ 7_CasosDeUso.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <variant>
+#include <stdexcept>
+
+using namespace std;
+
+class Entorno {
+public:
+    using Value = variant<int, double, string>;
+
+    Entorno() = default;
+
+    Entorno(const Entorno&) = delete;
+    Entorno& operator=(const Entorno&) = delete;
+
+    void setVariable(const string& name, const Value& value, bool readOnly = false) {
+        auto [iter, success] = simbolos.insert({name, make_pair(value, readOnly)});
+        if (!success && iter->second.second) {
+            throw runtime_error("No se puede modificar la constante del juego '" + name + "'");
+        } else if (!success) {
+            iter->second.first = value;
+        }
+    }
